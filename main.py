46b106a511f1dac6de72a5f3cdcfd962d16a406b
w-garcia <w.garcia@ufl.edu>
2019-03-13 17:05:50 -0400

    fix loss

--- main.py
+++ main.py
@@ -17,37 +17,36 @@ def sigmoid_prime(z):
 
 class Layer(object):
     def __init__(self, n_in, layer_size):
-        self.W = np.random.rand(layer_size, n_in)
+        self.W = 2 * np.random.random((layer_size, n_in)) - 1
         #         self.deltas = np.zeros(layer_size, n_in + 1)
         self.activations = np.zeros(layer_size)
-        self.learning_rate = 0.010
+        self.learning_rate = 0.1
 
     def z(self, x):
         return np.dot(self.W, x)
 
     def a(self, x):
-        self.activations = self.z(x)
-        res = sigmoid(self.activations)
+        res = sigmoid(self.z(x))
         return res
 
     def forward(self, x):
-        # b || x
         # x_tilda = np.concatenate([[1.0], x])
         res = self.a(x)
+        self.activations = res
 
         return res
 
-    def backward(self, delta):
-        delta = np.dot(self.W.T, delta) * self.grad()
-        self.W += self.learning_rate * np.dot(self.W.T, delta)
-        return delta
+    def error(self, delta):
+        return np.dot(delta, self.W.T) * sigmoid_prime(self.activations)
 
-    def grad(self):
-        return sigmoid_prime(self.activations)
+    def update_with_gradient(self, delta):
+        grad = np.dot(self.activations, delta)
+        self.W += self.learning_rate * grad
 
 
 class Net(object):
     def __init__(self, n_in):
+        # b || w
         self.layers = [
             Layer(n_in + 1, 3),  # Hidden layer
             Layer(3, 1)  # Output, a logistic regression fn
@@ -61,22 +60,36 @@ class Net(object):
         return prev_out[0]
 
     def loss(self, y, t):
-        return (y - t) ** 2
+        error = (y - t) * sigmoid_prime(self.layers[-1].activations)
+        return error[0]
 
     def backwards(self, loss):
+        # Calc deltas
+        deltas = [loss * sigmoid_prime(self.layers[-1].activations)[0]]
         for l in reversed(self.layers[:-1]):
-            loss = l.backward(loss)
+            deltas.append(l.error(deltas[-1]))
+
+        deltas.reverse()
+
+        # update with ggradients
+        for i, l in enumerate(self.layers):
+            l.update_with_gradient(deltas[i])
 
     def train_loop(self, X, t, epochs):
         # no batch
         for ep in range(epochs):
             loss = 0
             for x_i, t_i in zip(X, t):
+            # i = np.random.randint(len(X))
+            #     x_i = X[i]
+            #     t_i = t[i]
                 y_i = self.h(x_i)
                 loss += self.loss(y_i, t_i)
                 self.backwards(loss)
 
             avg_loss = loss / len(X)
+
+            # if ep % 1000 == 0:
             print("Loss epoch={}:\t{}".format(ep, avg_loss))
 
 
@@ -119,8 +132,12 @@ def main():
     # plt.show()
 
     F = Net(2)
-    F.train_loop(X, t, 30)
+    F.train_loop(X, t, 1000)
 
+    print("0 0:\t", F.h([0, 0]))
+    print("1 0:\t", F.h([1, 0]))
+    print("0 1:\t", F.h([0, 1]))
+    print("1 1:\t", F.h([1, 1]))
 
 
 if __name__ == '__main__':
